344a6d8189d5f02692625499a420af6af3916338
Chakrit Racchat <117844293+ckrca@users.noreply.github.com>
2024-01-07 18:09:00 +0700

    Add files via upload

---
+++ Example.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Example{
+    uint  a = 256;
+    uint8   b = 22;
+
+    constructor() {
+        a += 1;
+    }
+
+    int c = -127;
+    int8 d = -5;
+}
